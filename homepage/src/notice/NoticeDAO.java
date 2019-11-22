a4e20d04f86bcfef19c7e6d1a777b4b387549699
soldesk <soldesk@soldesk-PC>
2019-11-22 10:51:22 +0900

    모델1기반의 homepage

---
+++ homepage/src/notice/NoticeDAO.java
@@ -0,0 +1,51 @@
+package notice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class NoticeDAO {
+	
+	public int total(Map map) {
+		int total=0;
+		
+		return total;
+	}
+	public List<NoticeDTO> list(Map map){
+		List<NoticeDTO> list = new ArrayList<NoticeDTO>();
+		
+		return list;
+	}
+	public boolean create(NoticeDTO dto) {
+		boolean flag = false;
+		
+		return flag;
+	}
+	public NoticeDTO read(int no) {
+		NoticeDTO dto = null;
+		
+		
+		return dto;
+	}
+	public void upViewcnt(int no) {
+		
+	}
+	public boolean passCheck(Map map) {
+		boolean flag = false;
+		
+		return flag;
+	}
+	
+	public boolean update(NoticeDTO dto) {
+		boolean flag = false;
+		
+		return flag;
+	}
+	
+	public boolean delete(int no) {
+		boolean flag = false;
+		
+		return flag;
+	}	
+
+}
---
+++ homepage/src/notice/NoticeDTO.java
@@ -0,0 +1,67 @@
+package notice;
+
+public class NoticeDTO {
+    private int    no      ;
+    private String id      ;
+    private String title   ;
+    private String content ;
+    private String passwd  ;
+    private String wdate   ;
+	public NoticeDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public NoticeDTO(int no, String id, String title, String content, String passwd, String wdate) {
+		super();
+		this.no = no;
+		this.id = id;
+		this.title = title;
+		this.content = content;
+		this.passwd = passwd;
+		this.wdate = wdate;
+	}
+	@Override
+	public String toString() {
+		return "NoticeDTO [no=" + no + ", id=" + id + ", title=" + title + ", content=" + content + ", passwd=" + passwd
+				+ ", wdate=" + wdate + "]";
+	}
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	public String getWdate() {
+		return wdate;
+	}
+	public void setWdate(String wdate) {
+		this.wdate = wdate;
+	}
+    
+    
+
+}
